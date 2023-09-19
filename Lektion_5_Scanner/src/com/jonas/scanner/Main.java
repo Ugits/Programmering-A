e625b65f0c45eb82d408d1fbd6a8dc636bfafade
Jonas Ullvahn <ullvahn@gmail.com>
2023-09-19 13:14:51 +0200

    Scanner practice

--- Lektion_5_Scanner/src/com/jonas/scanner/Main.java
+++ Lektion_5_Scanner/src/com/jonas/scanner/Main.java
@@ -1,7 +1,48 @@
 package com.jonas.scanner;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        boolean isPlaying = true;
+
+        do {
+            int result = scannerNumber();
+
+            for (int i = 0; i < result; i++) {
+                System.out.println(i);
+                System.out.println("LOOPING...");
+            }
+
+        } while (isPlaying);
+
+
+        /*
         System.out.println("App is running");
+
+        scanner();
+
+        Test.scannerTwo();
+
+        String text = "Hej";
+        */
+    }
+
+    public static String scannerText(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insert some sentence");
+        String input = sc.nextLine();
+
+        return input;
+    }
+
+    public static int scannerNumber(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please insert number");
+
+        return sc.nextInt();
     }
 }
---
+++ Lektion_5_Scanner/src/com/jonas/scanner/Test.java
@@ -0,0 +1,15 @@
+package com.jonas.scanner;
+
+import java.util.Scanner;
+
+public class Test {
+
+
+
+    public static void scannerTwo(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insert some sentence");
+        sc.nextLine();
+    }
+}
