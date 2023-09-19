4da9a4d90a5024b93b90e10990191c4c4abc83a6
Jonas Ullvahn <ullvahn@gmail.com>
2023-09-19 13:33:58 +0200

    update basic

--- Lektion_5_Scanner/src/com/jonas/scanner/Main.java
+++ Lektion_5_Scanner/src/com/jonas/scanner/Main.java
@@ -1,48 +1,20 @@
 package com.jonas.scanner;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        boolean isPlaying = true;
+        Scanner sc = new Scanner(System.in);
 
         do {
-            int result = scannerNumber();
 
-            for (int i = 0; i < result; i++) {
-                System.out.println(i);
-                System.out.println("LOOPING...");
-            }
+            System.out.println("Input Number!");
+            int result = sc.nextInt();
 
-        } while (isPlaying);
+            System.out.println("You picked: " + result);
+        } while (true);
 
-
-        /*
-        System.out.println("App is running");
-
-        scanner();
-
-        Test.scannerTwo();
-
-        String text = "Hej";
-        */
-    }
-
-    public static String scannerText(){
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Insert some sentence");
-        String input = sc.nextLine();
-
-        return input;
-    }
-
-    public static int scannerNumber(){
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Please insert number");
-
-        return sc.nextInt();
     }
 }
