d1f91e03ded8a598da6491e8468398b003d776b6
Jonas Ullvahn <ullvahn@gmail.com>
2023-09-19 13:41:50 +0200

    student scanner implementation

--- Lektion_5_Scanner/src/com/jonas/scanner/Main.java
+++ Lektion_5_Scanner/src/com/jonas/scanner/Main.java
@@ -5,15 +5,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        //Objects
         Scanner sc = new Scanner(System.in);
+        Student benny = new Student();
 
+        // Game
         do {
 
-            System.out.println("Input Number!");
+            System.out.println("Input bennys age");
             int result = sc.nextInt();
 
+            benny.age = result;
+
             System.out.println("You picked: " + result);
+            System.out.println("Bennys age is: " + benny.age);
+            
         } while (true);
 
     }
---
+++ Lektion_5_Scanner/src/com/jonas/scanner/Student.java
@@ -0,0 +1,7 @@
+package com.jonas.scanner;
+
+public class Student {
+    public String name;
+    public int age;
+    public boolean isTierd;
+}
